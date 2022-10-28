f1e16104a053dfae008dca8486abea802ac793f9
bloody-slipper <anniershova@gmail.com>
2022-10-28 23:41:36 +0300

    initial commit

---
+++ src/test/java/ru/netology/bdd/page/MoneyTransferPage.java
@@ -0,0 +1,31 @@
+package ru.netology.bdd.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MoneyTransferPage {
+    private SelenideElement amountField = $x("//div[@data-test-id = 'amount']//input");
+    private SelenideElement fromField = $x("//span[@data-test-id = 'from']//input");
+    private SelenideElement buttonTrans = $x("//button[@data-test-id='action-transfer']");
+
+    private SelenideElement errorNotification = $x("//div[@data-test-id='error-notification']");
+
+    public DashboardPage transfer(int amount, String cardNumber) {
+        amountField.setValue(Integer.toString(amount));
+        fromField.setValue(cardNumber);
+        buttonTrans.click();
+        return new DashboardPage();
+    }
+
+    public void getNotification() {
+
+        errorNotification.shouldBe(Condition.visible);
+        errorNotification.shouldHave(Condition.exactText("Ошибка\n" +
+                "Ошибка! Произошла ошибка"));
+
+
+    }
+}
---
+++ src/test/java/ru/netology/bdd/test/TestTransfer.java
@@ -0,0 +1,64 @@
+package ru.netology.bdd.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.bdd.data.DataHelper;
+import ru.netology.bdd.page.DashboardPage;
+import ru.netology.bdd.page.LoginPage;
+import ru.netology.bdd.page.MoneyTransferPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestTransfer {
+    @BeforeEach
+    public void shouldLogin() {
+
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+    @Test
+    public void shouldTransactionFromFirstToSecond() {
+        Configuration.holdBrowserOpen = true;
+        var dashboardPage = new DashboardPage();
+        var firstBalance = dashboardPage.getFirstCardBalance();
+        var secondBalance = dashboardPage.getSecondCardBalance();
+        int cardIndex = 1;
+        int amount = 150;
+        dashboardPage.transferTo(cardIndex).transfer(amount, DataHelper.getFirstCardNumber().getNumber());
+        var currentBalanceFirst = dashboardPage.getFirstCardBalance();
+        var currentBalanceSecond = dashboardPage.getSecondCardBalance();
+        Assertions.assertEquals(firstBalance - amount, currentBalanceFirst);
+        Assertions.assertEquals(secondBalance + amount, currentBalanceSecond);
+    }
+    @Test
+    public void shouldTransactionFromSecondToFirst() {
+        Configuration.holdBrowserOpen = true;
+        var dashboardPage = new DashboardPage();
+        var firstBalance = dashboardPage.getFirstCardBalance();
+        var secondBalance = dashboardPage.getSecondCardBalance();
+        int cardIndex = 0;
+        int amount = 250;
+        dashboardPage.transferTo(cardIndex).transfer(amount, DataHelper.getSecondCardNumber().getNumber());
+        var currentBalanceFirst = dashboardPage.getFirstCardBalance();
+        var currentBalanceSecond = dashboardPage.getSecondCardBalance();
+        Assertions.assertEquals(firstBalance + amount, currentBalanceFirst);
+        Assertions.assertEquals(secondBalance - amount, currentBalanceSecond);
+    }
+    @Test
+    public void shouldReturnErrorWithInvalidCard() {
+        Configuration.holdBrowserOpen = true;
+        var dashboardPage = new DashboardPage();
+        int cardIndex = 0;
+        int amount = 150;
+        var page = dashboardPage.transferTo(cardIndex);
+        page.transfer(amount, DataHelper.getInvalidCardNumber().getNumber());
+        page.getNotification();
+
+    }
+}
